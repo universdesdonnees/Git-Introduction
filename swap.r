b812effc15f7d201c3329cf9202d117770281cc5
universdesdonnees <122817372+universdesdonnees@users.noreply.github.com>
2023-12-07 21:49:51 +0100

    new swap

--- swap.r
+++ swap.r
@@ -4,7 +4,9 @@ library(ggplot2)
 # Sample data
 data <- data.frame(
   x = 1:10,
-  y = c(2, 4, 3, 6, 5, 8, 7, 10, 9, 12)
+  z = 1:10,
+  y = c(2, 4, 3, 6, 5, 8, 7, 10, 9, 12), 
+  d = c(1,4)
 )
 
 # Create a scatter plot
